46282d297e3f0842bf6697e28ed87aa79f5dbdc6
Naveli Choudhary <Naveli.Choudhary-ext@bshg.com>
2022-05-08 18:21:03 +0530

    Added basic modal

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+// import React from "react";
 
-function Home() {
-  return <div>Home</div>;
+// function Home() {
+//   return <div><button>Hi</button></div>;
+// }
+
+// export default Home;
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import JiraIcon from './icon.png';
+
+export default function Home() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Sign in
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>Sign In</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Continue with..&nbsp;&nbsp;&nbsp;
+            <GitHubIcon fontSize="large" />
+            &nbsp;&nbsp;
+            <img src={JiraIcon} width={30} height={30} alt="BigCo Inc. logo"/>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
 }
-
-export default Home;
