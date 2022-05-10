6cb756212b44cb12d0526d54c76dbcc594bb8b01
Naveli Choudhary <Naveli.Choudhary-ext@bshg.com>
2022-05-10 12:53:19 +0530

    Github OAuth Fixes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,6 @@
-import React, {useEffect} from "react";
-import Axios from "axios";
+import React from "react";
 
 function Home(props) {
-
-  useEffect(() => {
-    const search = props.location.search;
-    const code = new URLSearchParams(search).get("code");
-    Axios.post('http://localhost:8080/auth/github/accesstoken', { code }).then(res => {
-      console.log(res);
-    })
-  });
   
   return <div>Home</div>;
 }
