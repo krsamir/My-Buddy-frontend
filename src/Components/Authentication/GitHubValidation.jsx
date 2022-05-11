e60b680acb50961ce61c83d1e14f934d5206b835
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2022-05-11 11:29:09 +0530

    Adding validation screen

--- src/Components/Authentication/GitHubValidation.jsx
+++ src/Components/Authentication/GitHubValidation.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 import Axios from "axios";
 
 function GitHubValidation(props) {
@@ -12,12 +14,33 @@ function GitHubValidation(props) {
       { params },
       { withCredentials: true }
     ).then((res) => {
-      if (res.data.status === 1) props.history.push("/");
-      else props.history.push("/login");
+      if (res.data.status === 1) {
+        setTimeout(() => {
+          props.history.push("/");
+        }, 500);
+      } else {
+        setTimeout(() => {
+          props.history.push("/login");
+        }, 500);
+      }
     });
-  });
 
-  return <div>Please wait while we log you in!!</div>;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      {" "}
+      <div>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={true}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </div>
+    </div>
+  );
 }
 
 export default GitHubValidation;
